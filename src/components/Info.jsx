0571dd811d62d1cff9cd6eab82d2d4bde7bf4479
Matias Mendez <mati06mendez@gmail.com>
2025-12-06 22:39:58 -0300

    Info section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,14 @@
+import separador from "../assets/Guardapampaentera.png"
+import fediap from "../assets/fediap-logo.png"
 import "./Info.css";
 
 export default function Info() {
     return (
-        <section></section>
+        <section className="info">
+            <img src={separador} alt="espaciador" className="separador"/>
+            <img src={fediap} alt="" className="fediap" />
+            <p>Las Jornadas Regionales de FEDIAP Destinado a docentes y técnicos de escuelas agrotécnicas de todo el país, durante tres días intensivos se realizarán actividades de exposición de experiencias, presentación de casos, disertaciones y networking.</p>
+            <hr />
+        </section>
     );
 }
