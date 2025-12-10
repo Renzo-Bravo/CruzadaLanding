3d39d91637b7f657409b057ba59f5d8fc5be80aa
Renzo-Bravo <renzobravogomez@gmail.com>
2025-12-10 08:05:33 -0300

    View in Tablets is OK

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,7 +8,7 @@ export default function Info() {
         <section className="info" id="eventContent">
             <img src={separadorEnt} alt="Espaciador" className="separadorEnt"/>
             <img src={separadorMed} alt="Espaciador" className="separadorMed" />
-            <img src={fediap} alt="" className="fediap" />
+            <img src={fediap} alt="Fedial" className="fediap" />
             <p>Las Jornadas Regionales de FEDIAP Destinado a docentes y técnicos de escuelas agrotécnicas de todo el país, durante tres días intensivos se realizarán actividades de exposición de experiencias, presentación de casos, disertaciones y networking.</p>
             <hr />
         </section>
